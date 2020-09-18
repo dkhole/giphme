109f46cb5b382519732c789af7164038c09ab98d
dkhole <dkhlee419@gmail.com>
2020-09-18 14:49:12 +1000

    async refractor

--- script.js
+++ script.js
@@ -2,20 +2,17 @@ const apiKey = '6flguaZ4XstDNrv8tZpNurgUyFWCh3x6';
 const weirdness = 10;
 
 
-function getGif(apiKey, search, weird) {
+async function getGif(apiKey, search, weird) {
     const img = document.querySelector('img');
 
-    fetch(`http://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${search}&weirdness=${weird}`, { mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        img.src = response.data.images.original.url;
-    })
-    .catch(function(e) {
-        console.log(e);
-    });
+    const response = await fetch(`http://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${search}&weirdness=${weird}`, { mode: 'cors'})
+    const gif = await response.json()
+    img.src = gif.data.images.original.url;
 }
+
+getGif().catch(function(e) {
+    console.log(e);
+})
 
 const searchDom = document.querySelector('#search');
 
